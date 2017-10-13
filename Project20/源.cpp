cc933c9c87948a043602dc07a805a372a01a11d6
1225993807 <32097625+1225993807@users.noreply.github.com>
2017-10-13 21:29:22 +0800

    添加项目文件。

---
+++ Project20/源.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	void A();
+	void B();
+	void C();
+
+lable_1:int a;
+	printf("请输入1或2或3.\n");
+	scanf("%d",&a);
+
+	switch (a)
+	{
+	case 1:A(); goto lable_1;
+	case 2:B(); goto lable_1;
+	case 3:C(); goto lable_1;
+	default:break;
+	}
+	return 0;
+
+}
+
+void A()
+{
+	printf("A函数\n");
+}
+
+void B()
+{
+	printf("B函数\n");
+}
+
+void C()
+{
+	printf("C函数\n");
+}
